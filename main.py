da4b149e3817dcbe161a1a9fcd3fc071394ad163
K0DkodK0D <fabriziogentile07@gmail.com>
2025-11-12 18:40:49 +0100

    Delete main.py

--- main.py
+++
@@ -1,4 +0,0 @@
-from OpenAI_services import generaRisposta
-#Main    : Establish connection with Arduino
-#THREAD 1: Wait for wakeword -> Mic input on azure STT -> OpenAI API call -> Azure TTS output on speaker
-#THREAD 2: Read distance values from Arduino -> Move accordingly (either following line or idling)
