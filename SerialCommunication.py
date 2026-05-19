3b2cd3d36b21a30033bbb35eeb4c45d77e885292
K0DkodK0D <fabriziogentile07@gmail.com>
2026-05-19 07:43:19 +0200

    Update SerialCommunication.py

--- SerialCommunication.py
+++ SerialCommunication.py
@@ -1,6 +1,15 @@
 import serial
+from Audio import speak
 
-Arduino = serial.Serial(port='COM4', baudrate=9600, timeout=1)
+try:
+    Arduino = serial.Serial(port='COM4', baudrate=9600, timeout=5)
+    speak("Connessione alle periferiche riuscita.")
+except serial.SerialException:
+    speak("Nessun dispositivo seriale trovato. Modalità test attiva.")
+    Arduino = None
+
+def readDistance():
+    return Arduino.readline().decode("utf-8").strip()
 
 def send(comando):
-    Arduino.write(comando.encode())
+    Arduino.write((chr(ord('a')+comando).encode("utf-8")))
